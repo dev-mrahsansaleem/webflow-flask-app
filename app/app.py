bac7d387299aaf24e671f0bed9c5e33c26c21bcf
dev.mrahsansaleem <ahsan.saleem@vicenna.com>
2022-07-20 19:22:32 +0500

    create store data and hit url

--- app/app.py
+++ app/app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def test():
+    return jsonify("working")
+
+@app.route("/")
+def storeData():
+    url = request.args.get('url')
+    r = requests.get(url = url)
+    
+    # extracting data in json format
+    data = r.json()
+    return jsonify(data)
