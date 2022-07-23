748943c2f7c5d7b27021a22488cfd19fdb8253aa
dev.mrahsansaleem <ahsan.saleem@vicenna.com>
2022-07-23 09:27:00 +0500

    change key name

--- app/routes/route.py
+++ app/routes/route.py
@@ -29,82 +29,82 @@ def sendImage():
     # data = re.sub('\s+',"",data) # remove all spaces
     arrData = re.split('<\/?script>',get_resp.text) # split by script open close tags
     data = [x for x in arrData if "props" in x]
-    if len(data) > 0 :
-        start = data[0].find('"require"') 
-        end = data[0].rfind(',"contexts')
-        data = '{'+data[0][start: end]+'}'
-        data = re.sub('\\"',"\"",data) # replace \qoutes with qoutes
-        dataJson = json.loads(data)
-        props = dataJson['require'][10][3][0]['props']
-        
-        data = {
-            "fields": {
-                "name": props['deeplinkAdID'], #need to change to find ad title always
-                "slug": props['deeplinkAdID'],
-                "ad-id": props['deeplinkAdID'],
-                "ad-title": props['deeplinkAdCard']['snapshot']["cards"][0]["title"] if len(props['deeplinkAdCard']['snapshot']["cards"]) > 0 else "",
-                "ad-body": props['deeplinkAdCard']['snapshot']["cards"][0]["body"] if len(props['deeplinkAdCard']['snapshot']["cards"]) > 0 else "",
-                "ad-caption": props['deeplinkAdCard']['snapshot']["cards"][0]["caption"] if len(props['deeplinkAdCard']['snapshot']["cards"]) > 0 else "",
-                "ad-resized-image": "",
-                "ad-original-image": "",
-                "ad-page-creation-time": datetime.fromtimestamp(props['deeplinkAdCard']['snapshot']["creation_time"]).strftime("%d-%b-%Y"), # (%H:%M:%S.%f)
-                "ad-page-like-count": str(props['deeplinkAdCard']['snapshot']["page_like_count"]),
-                "ad-type": props['adType'],
-                "is-active": str(props['deeplinkAdCard']['isActive']),
-                "link-url": props['deeplinkAdCard']['snapshot']["link_url"],
-                "page-id": str(props['deeplinkAdCard']['pageID']),
-                "page-name": props['deeplinkAdCard']['pageName'],
-                "profile-picture": "",
-                "page-categories": " ".join([item for item in props['deeplinkAdCard']['snapshot']['page_categories'].values()]),
-                "page-url": props['deeplinkAdCard']['snapshot']["page_profile_uri"],
-                "media-type": props['mediaType'],
-                "publisher-platform": " ".join([item for item in props['deeplinkAdCard']['publisherPlatform']]),
-                "_archived": False,
-                "_draft": False,
-                },
-            }
-        # profile picture store
-        imagObj = Image.open(BytesIO(requests.get(props['deeplinkAdCard']['snapshot']['page_profile_picture_url']).content))
-        image_name = 'profile_' + props['deeplinkAdID'] + "."+ imagObj.format.lower()
-        imagObj.save(app.config['UPLOAD_FOLDER'] + image_name)
-        data['fields']['profile-picture']  = app.config['BASE_APP_URL'] + image_name
-
-        if len(props['deeplinkAdCard']['snapshot']["cards"]) > 0:
-            # resized image
-            imagObj = Image.open(BytesIO(requests.get(props['deeplinkAdCard']['snapshot']["cards"][0]["resized_image_url"]).content))
-            image_name = 'resized_' + props['deeplinkAdID'] + "."+ imagObj.format.lower()
-            imagObj.save(app.config['UPLOAD_FOLDER'] + image_name)
-            data['fields']['ad-resized-image']  = app.config['BASE_APP_URL'] + image_name
-                
-            # orignal image
-            imagObj = Image.open(BytesIO(requests.get(props['deeplinkAdCard']['snapshot']["cards"][0]["original_image_url"]).content))
-            image_name = 'orignal_' + props['deeplinkAdID'] + "."+ imagObj.format.lower()
-            imagObj.save(app.config['UPLOAD_FOLDER'] + image_name)
-            data['fields']['ad-original-image']  = app.config['BASE_APP_URL'] + image_name
-
-        # return jsonify(data)
-
-        # pprint(params)
-        headers = {
-        'content-type': 'application/json',
-        "Authorization":"Bearer f789b409bf0afe37e052522a4df7cdc1f99fe908d20ac4043df7e831d629a97e"
+    if len(data) <= 0 :
+        return jsonify({"data":data,"msg":"erro"})
+    
+    start = data[0].find('"require"') 
+    end = data[0].rfind(',"contexts')
+    data = '{'+data[0][start: end]+'}'
+    data = re.sub('\\"',"\"",data) # replace \qoutes with qoutes
+    dataJson = json.loads(data)
+    props = dataJson['require'][10][3][0]['props']
+    
+    data = {
+        "fields": {
+            "name": props['deeplinkAdID'], #need to change to find ad title always
+            "slug": props['deeplinkAdID'],
+            "ad-id": props['deeplinkAdID'],
+            "ad-title": props['deeplinkAdCard']['snapshot']["cards"][0]["title"] if len(props['deeplinkAdCard']['snapshot']["cards"]) > 0 else "",
+            "ad-body": props['deeplinkAdCard']['snapshot']["cards"][0]["body"] if len(props['deeplinkAdCard']['snapshot']["cards"]) > 0 else "",
+            "ad-caption": props['deeplinkAdCard']['snapshot']["cards"][0]["caption"] if len(props['deeplinkAdCard']['snapshot']["cards"]) > 0 else "",
+            "ad-resized-image": "",
+            "ad-original-image": "",
+            "ad-page-creation-time": datetime.fromtimestamp(props['deeplinkAdCard']['snapshot']["creation_time"]).strftime("%d-%b-%Y"), # (%H:%M:%S.%f)
+            "ad-page-like-count": str(props['deeplinkAdCard']['snapshot']["page_like_count"]),
+            "ad-type": props['adType'],
+            "is-active": str(props['deeplinkAdCard']['isActive']),
+            "link-url": props['deeplinkAdCard']['snapshot']["link_url"],
+            "page-id": str(props['deeplinkAdCard']['pageID']),
+            "page-name": props['deeplinkAdCard']['pageName'],
+            "profile-picture": "",
+            "page-categories": " ".join([item for item in props['deeplinkAdCard']['snapshot']['page_categories'].values()]),
+            "page-url": props['deeplinkAdCard']['snapshot']["page_profile_uri"],
+            "media-type": props['mediaType'],
+            "publisher-platform": " ".join([item for item in props['deeplinkAdCard']['publisherPlatform']]),
+            "_archived": False,
+            "_draft": False,
+            },
         }
-        dump = json.dumps(data)
-        post_resp = requests.post(url = "https://api.webflow.com/collections/62daf04048a18d41dc374c6f/items?live=true",data=dump,headers=headers)
-        sitePublishQueued = False
-        if post_resp.status_code == 200:
-            dump = json.dumps({"domains": app.config['SITE_ID']})
-            resp = requests.post(url = "https://api.webflow.com/sites/"+ app.config['SITE_ID'] + "/publish",data=dump,headers=headers)
-            sitePublishQueued = resp.json()
+    # profile picture store
+    imagObj = Image.open(BytesIO(requests.get(props['deeplinkAdCard']['snapshot']['page_profile_picture_url']).content))
+    image_name = 'profile_' + props['deeplinkAdID'] + "."+ imagObj.format.lower()
+    imagObj.save(app.config['UPLOAD_FOLDER'] + image_name)
+    data['fields']['profile-picture']  = app.config['BASE_APP_URL'] + image_name
 
-        return jsonify({
-            "code":post_resp.status_code,
-            "msg":'created',
-            "data":post_resp.json(),
-            "sitePublishQueued" : sitePublishQueued
-            })
-    else:
-        jsonify({"data":data,"msg":"erro"})
+    if len(props['deeplinkAdCard']['snapshot']["cards"]) > 0:
+        # resized image
+        imagObj = Image.open(BytesIO(requests.get(props['deeplinkAdCard']['snapshot']["cards"][0]["resized_image_url"]).content))
+        image_name = 'resized_' + props['deeplinkAdID'] + "."+ imagObj.format.lower()
+        imagObj.save(app.config['UPLOAD_FOLDER'] + image_name)
+        data['fields']['ad-resized-image']  = app.config['BASE_APP_URL'] + image_name
+            
+        # orignal image
+        imagObj = Image.open(BytesIO(requests.get(props['deeplinkAdCard']['snapshot']["cards"][0]["original_image_url"]).content))
+        image_name = 'orignal_' + props['deeplinkAdID'] + "."+ imagObj.format.lower()
+        imagObj.save(app.config['UPLOAD_FOLDER'] + image_name)
+        data['fields']['ad-original-image']  = app.config['BASE_APP_URL'] + image_name
+
+    # return jsonify(data)
+
+    # pprint(params)
+    headers = {
+    'content-type': 'application/json',
+    "Authorization":"Bearer f789b409bf0afe37e052522a4df7cdc1f99fe908d20ac4043df7e831d629a97e"
+    }
+    dump = json.dumps(data)
+    post_resp = requests.post(url = "https://api.webflow.com/collections/62daf04048a18d41dc374c6f/items?live=true",data=dump,headers=headers)
+    sitePublishQueued = False
+    if post_resp.status_code == 200:
+        dump = json.dumps({"domains": app.config['SITE_ID']})
+        resp = requests.post(url = "https://api.webflow.com/sites/"+ app.config['SITE_ID'] + "/publish",data=dump,headers=headers)
+        sitePublishQueued = resp.json()
+
+    return jsonify({
+        "code":post_resp.status_code,
+        "msg":'created',
+        "data":post_resp.json(),
+        "sitePublishQueued" : sitePublishQueued
+        })
 
 
 
